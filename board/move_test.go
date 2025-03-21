49625afd4e1be7251c3c9e0a8cb5d853943c0eaa
MSWS <imodmaker@gmail.com>
2025-03-21 03:34:47 -0700

    Refactor and enhance chess piece movement tests
    - Remove unnecessary print statement for setting color to black
    - Refactor tests to consolidate color scenario handling
    - Simplify pawn and knight move tests for color variation
    - Add tests for other pieces from the starting position
    - Improve test organization for enhanced readability and maintainability

--- board/move_test.go
+++ board/move_test.go
@@ -1,7 +1,6 @@
 package board
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -9,7 +8,7 @@ func getStartMovesForType(piece Piece) []Move {
 	start := getStartGame()
 
 	if piece.GetColor()&Black == Black {
-		fmt.Printf("setting color to black")
+		start.Active = Black
 	}
 
 	result := []Move{}
@@ -27,26 +26,53 @@ func getStartMovesForType(piece Piece) []Move {
 
 func TestGetMoves(t *testing.T) {
 	t.Run("Starting Position", func(t *testing.T) {
-		start := getStartGame()
+		testForBothColors(t, func(color Piece) {
+			start := getStartGame()
+			start.Active = color
 
-		result := start.GetMoves()
+			result := start.GetMoves()
 
-		if len(result) != 20 {
-			t.Errorf("invalid number of legal moves, expected %d, got %d\n%v", 20, len(result), result)
-		}
+			if len(result) != 20 {
+				t.Errorf("invalid number of legal moves, expected %d, got %d\n%v", 20, len(result), result)
+			}
+		})
 	})
 
 	t.Run("Pawn Moves", func(t *testing.T) {
-		pawnMoves := getStartMovesForType(Pawn)
-		if len(pawnMoves) != 16 {
-			t.Errorf("invalid number of pawn moves, expected %d, got %d\n%v", 16, len(pawnMoves), pawnMoves)
-		}
+		testForBothColors(t, func(color Piece) {
+			pawnMoves := getStartMovesForType(Pawn | color)
+			if len(pawnMoves) != 16 {
+				t.Errorf("invalid number of pawn moves, expected %d, got %d\n%v", 16, len(pawnMoves), pawnMoves)
+			}
+		})
 	})
 
 	t.Run("Knight Moves", func(t *testing.T) {
-		knightMoves := getStartMovesForType(Knight)
-		if len(knightMoves) != 4 {
-			t.Errorf("invalid number of knight moves, expected %d, got %d\n%v", 4, len(knightMoves), knightMoves)
-		}
+		testForBothColors(t, func(color Piece) {
+			knightMoves := getStartMovesForType(Knight | color)
+			if len(knightMoves) != 4 {
+				t.Errorf("invalid number of knight moves, expected %d, got %d\n%v", 4, len(knightMoves), knightMoves)
+			}
+		})
+	})
+
+	for _, piece := range []Piece{Rook, Bishop, Queen, King} {
+		t.Run(string(piece.GetRune()), func(t *testing.T) {
+			testForBothColors(t, func(color Piece) {
+				moves := getStartMovesForType(piece | color)
+				if len(moves) != 0 {
+					t.Errorf("invalid number of knight moves, expected %d, got %d\n%v", 0, len(moves), moves)
+				}
+			})
+		})
+	}
+}
+
+func testForBothColors(t *testing.T, test func(color Piece)) {
+	t.Run("White", func(t *testing.T) {
+		test(White)
+	})
+	t.Run("Black", func(t *testing.T) {
+		test(Black)
 	})
 }
