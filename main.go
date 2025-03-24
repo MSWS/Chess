8e30f7db4c1b6b0a7419b64de14ee506f6f9b534
MSWS <imodmaker@gmail.com>
2025-03-24 12:06:14 -0700

    Revert "feat: Implement UCI engine with command processing loop"
    This reverts commit f83f9dd036528241517e0105e2d239e3d55fa3a6.

--- main.go
+++ main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
-	"github.com/msws/chess/uci"
+	"github.com/msws/chess/board"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	engine := uci.NewEngine(reader, os.Stdout)
+	board, err := board.FromFEN("r3k2r/p1pPqpb1/1n3np1/1b2N3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R b KQkq - 0 2")
+	if err != nil {
+		panic(err)
+	}
 
-	for {
-		cmd, err := reader.ReadString('\n')
+	baseMoves := board.GetMoves()
 
+	depth := 1
+	total := 0
+
+	println(strings.Join(os.Args, ","))
+
+	if len(os.Args) == 2 {
+		val, err := strconv.Atoi(os.Args[1])
 		if err != nil {
 			panic(err)
 		}
-
-		go engine.Print(cmd)
+		depth = val
 	}
+
+	// fmt.Printf("Depth: %d\n", depth)
+
+	for _, move := range baseMoves {
+		board.MakeMove(move)
+
+		perft := board.Perft(depth - 1)
+		total += perft
+		fmt.Printf("%v%v: %d\n", move.From.GetAlgebra(), move.To.GetAlgebra(), perft)
+
+		board.UndoMove()
+	}
+
+	fmt.Printf("Depth: %d, (%d)", depth, total)
 }
